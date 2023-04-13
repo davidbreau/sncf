ebd5168677373d6f74ffd6a10abbc94e883e57e7
davidbreau <davidbreau@live.fr>
2023-04-13 11:33:04 +0200

    bugfix visualisation

--- F_visualisation.py
+++ F_visualisation.py
@@ -31,7 +31,7 @@ df_filtered = df[df['type'].isin(selected_types)]
 df_weekly = df_filtered.groupby(pd.Grouper(key='date', freq='W')).count()['id'].reset_index()
 
 # Créer un histogramme avec Plotly
-fig = px.bar(df_weekly, x='date', y='id')
+fig = px.bar(df_weekly, x='date', y='id', color='id', color_continuous_scale='Plasma')
 
 # Afficher le graphique avec Streamlit
 st.plotly_chart(fig)
@@ -72,6 +72,23 @@ df_nb_objets_par_gare = df_objet_filtre.groupby('gare')['id'].count().reset_inde
 # Calcul du ratio fréquentation / nombre d'objets trouvés
 df_gare['ratio_freq_objets'] = round((df_nb_objets_par_gare.nb_objets * 1_000_000 / df_gare['frequentation_' + annee_selectionnee]))
 
+import colorcet as cc
+
+def plasma_color(value, min_value, max_value):
+    value_normalized = (value - min_value) / (max_value - min_value)
+    color_index = int(value_normalized * 255)
+    hex_color = cc.palette["CET_CBD2"][color_index]
+    rgb_color = tuple(int(hex_color[i:i+2], 16) for i in (1, 3, 5))
+    return list(rgb_color)
+
+# Calcul du ratio fréquentation / nombre d'objets trouvés
+df_gare['ratio_freq_objets'] = round((df_nb_objets_par_gare.nb_objets * 1_000_000 / df_gare['frequentation_' + annee_selectionnee]))
+
+# Générer les couleurs Plasma pour chaque gare en fonction du ratio
+min_ratio = df_gare['ratio_freq_objets'].min()
+max_ratio = df_gare['ratio_freq_objets'].max()
+df_gare['plasma_color'] = df_gare['ratio_freq_objets'].apply(lambda x: plasma_color(x, min_ratio, max_ratio))
+df_gare['rgb_color'] = df_gare['plasma_color'].apply(lambda x: tuple(x))
 
 # Création de la carte de Paris avec les barres représentant le ratio
 view_state = pdk.ViewState(
@@ -86,8 +103,7 @@ layer = pdk.Layer(
     get_position='[lon, lat]',
     get_elevation='ratio_freq_objets',
     elevation_scale=10,
-    # get_fill_color='[255 * (1 - ratio_freq_objets), 0, 255 * ratio_freq_objets]',
-    get_fill_color='[255, ratio_freq_objets, ratio_freq_objets, 175]',
+    get_fill_color='rgb_color',
     pickable=True,
     auto_highlight=True,
     extruded=True,
@@ -99,3 +115,42 @@ tooltip = {
 }
 r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip=tooltip)
 st.pydeck_chart(r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Création de la carte de Paris avec les barres représentant le ratio
+# view_state = pdk.ViewState(
+#     longitude=df_gare['lon'].mean(),
+#     latitude=df_gare['lat'].mean(),
+#     zoom=12,
+#     pitch=45
+# )
+# layer = pdk.Layer(
+#     'ColumnLayer',
+#     data=df_gare,
+#     get_position='[lon, lat]',
+#     get_elevation='ratio_freq_objets',
+#     elevation_scale=10,
+#     # get_fill_color='[255 * (1 - ratio_freq_objets), 0, 255 * ratio_freq_objets]',
+#     get_fill_color='[255, ratio_freq_objets, ratio_freq_objets, 175]',
+#     pickable=True,
+#     auto_highlight=True,
+#     extruded=True,
+#     coverage=0.2
+# )
+# tooltip = {
+#     "html": "<b>Gare :</b> {gare}<br/><b>Nombre d'objets trouvés sur 1 million de voyageurs :</b> {ratio_freq_objets} objets",
+#     "style": {"backgroundColor": "black", "color": "white"}
+# }
+# r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip=tooltip)
+# st.pydeck_chart(r)
--- G_data_science.py
+++ G_data_science.py
@@ -3,36 +3,6 @@ import pandas as pd
 import sqlite3
 import plotly.express as px
 
-def load_data():
-    conn = sqlite3.connect('bdd.db')
-    query = """
-    SELECT o.date, t.temperature, COUNT(o.id) as nombre_objets
-    FROM objet o
-    JOIN temperature t ON o.date = t.date
-    GROUP BY o.date, t.temperature
-    """
-    df = pd.read_sql_query(query, conn)
-    conn.close()
-    return df
-
-df = load_data()
-
-st.title("Analyse des objets trouvés en fonction de la température")
-
-fig = px.scatter(df, x='temperature', y='nombre_objets', trendline="ols")
-st.plotly_chart(fig)
-
-st.write("Est-ce que le nombre d'objets perdus est corrélé à la température d'après ce graphique?")
-st.write("Si le graphique montre une tendance claire (ascendante ou descendante) entre la température et le nombre d'objets trouvés, il y a une corrélation entre les deux variables. Sinon, il n'y a pas de corrélation évidente.")
-
-
-####################################################################################################
-
-
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-import sqlite3
 
 def load_data():
     conn = sqlite3.connect("bdd.db")
@@ -56,18 +26,13 @@ st.title("Analyse des objets trouvés en fonction de la température")
 df = load_data()
 df_filtre = prepare_data(df)
 
-fig = px.scatter(df_filtre, x='temperature', y='nombre_objets', trendline="ols")
-
+fig = px.scatter(df_filtre, x='temperature', y='nombre_objets', color='temperature', color_continuous_scale='Plasma')
 st.write(fig)
 
-results = px.get_trendline_results(fig)
-ols_results = results.iloc[0]['px_fit_results']
-
 st.write("Est-ce que le nombre d'objets perdus est corrélé à la température d'après ce graphique?")
-st.write(f"La p-value pour cette relation est {ols_results.pvalues[1]:.6f}.")
 
-if ols_results.pvalues[1] < 0.05:
-    st.write("Oui, le nombre d'objets perdus est significativement corrélé à la température.")
-else:
-    st.write("Non, le nombre d'objets perdus n'est pas significativement corrélé à la température.")
+
+
+#################################################################
+
 
