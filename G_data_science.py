2c02f4637146bb6a10e7c958da446770eb882151
davidbreau <davidbreau@live.fr>
2023-04-13 22:49:57 +0200

    data science 2/3

--- G_data_science.py
+++ G_data_science.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 import plotly.express as px
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def load_data():
     conn = sqlite3.connect("bdd.db")
@@ -35,5 +36,22 @@ st.write("Est-ce que le nombre d'objets perdus est corrélé à la température 
 
 #################################################################
 
-st.text('Quelle est la médiane du nombre d’objets trouvés en fonction de la saison? Il y a t il une correlation entre ces deux variables d\'après le graphique?')
-df
+
+st.title("Distribution du nombre d'objets trouvés par saison")
+
+#la fonction d'attribution des saisons sur chaque objet étant longue, un csv a été enregistré
+#afin d'alléger la génération de ce streamlit
+df = pd.read_csv('liste_objets_avec_saison.csv')
+# Création du sélecteur d'année
+annee = st.selectbox("Sélectionnez l'année", ["2019", "2020", "2021", "2022"])
+
+# Filtrage des données en fonction de l'année sélectionnée
+df_filtre = df[df['date'].str[-4:] == annee]
+
+# Calcul du nombre d'objets trouvés pour chaque saison et chaque type
+counts = df_filtre.groupby(['saison', 'type']).size().reset_index(name='count')
+
+
+# Création du boxplot avec Plotly Express
+box = px.box(counts, x='saison', y='count', color='saison', color_discrete_sequence=px.colors.sequential.Plasma)
+st.plotly_chart(box)
