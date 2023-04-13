53a794999ad95322eaa4849c338b4ae7748a5318
davidbreau <davidbreau@live.fr>
2023-04-13 10:09:17 +0200

    Visualisation finie

--- F_visualisation.py
+++ F_visualisation.py
@@ -3,6 +3,7 @@ import numpy as np
 import plotly.express as px
 import streamlit as st
 import sqlite3
+import pydeck as pdk
 
 ####################
 #################### HISTOGRAMME
@@ -18,6 +19,7 @@ df['date'] = pd.to_datetime(df['date'], format='%d/%m/%Y')
 # Créer une liste des types d'objet uniques
 types_objet = df['type'].unique()
 
+st.title("Nombre d'objets trouvés par semaine (total)")
 # Sélectionner les types d'objet à afficher
 selected_types = st.multiselect('Sélectionner les types d\'objet à afficher', types_objet, default=types_objet[:])
 
@@ -38,6 +40,9 @@ st.plotly_chart(fig)
 #################### CARTE
 ####################
 
+# Connexion à la base de données
+connexion = sqlite3.connect('bdd.db')
+
 # Récupération des données de la table objet
 query_objet = "SELECT * FROM objet"
 df_objet = pd.read_sql_query(query_objet, connexion)
@@ -49,29 +54,48 @@ df_gare = pd.read_sql_query(query_gare, connexion)
 # Fermeture de la connexion à la base de données
 connexion.close()
 
-# Création de la liste des années disponibles
-annees = ['2019', '2020', '2021', '2022']
-
-# Création de la liste des types d'objets disponibles
-types = df_objet['type'].unique().tolist()
-
 # Sélection de l'année et du type d'objet avec des widgets Streamlit
+annees = ['2019', '2020', '2021', '2022']
 annee_selectionnee = st.selectbox("Sélectionnez l'année :", annees, key=1)
-types_selectionnes = st.multiselect("Sélectionnez le type d'objet :", types, default=types[:], key=3)
+
+st.title("Nombre d'objets trouvés par année pour 1 million de voyageurs")
+types = df_objet['type'].unique().tolist()
+types_selectionnes = st.multiselect("Sélectionnez les types d'objets :", types, default=types[:], key=3)
 
 # Filtrage des données en fonction de la sélection de l'utilisateur
 df_objet_filtre = df_objet[(df_objet['type'].isin(types_selectionnes)) & (df_objet['date'].str[-4:] == annee_selectionnee)]
 df_gare_filtre = df_gare[['gare', 'lat', 'lon', 'frequentation_' + annee_selectionnee]]
 
-# Jointure des deux tables sur la colonne 'gare'
-df = pd.merge(df_objet_filtre, df_gare_filtre, on='gare')
+# Calcul du nombre d'objets trouvés des types sélectionnés pour chaque gare
+df_nb_objets_par_gare = df_objet_filtre.groupby('gare')['id'].count().reset_index().rename(columns={'id': 'nb_objets'})
 
-# Regroupement des données par gare pour obtenir le nombre total d'objets trouvés
-df_grouped = df.groupby(['gare', 'lat', 'lon', 'frequentation_' + annee_selectionnee]).size().reset_index(name='nombre_objets')
+# Calcul du ratio fréquentation / nombre d'objets trouvés
+df_gare['ratio_freq_objets'] = round((df_nb_objets_par_gare.nb_objets * 1_000_000 / df_gare['frequentation_' + annee_selectionnee]))
 
-# Affichage de la carte avec Plotly Express
-fig = px.scatter_mapbox(df_grouped, lat="lat", lon="lon", hover_name="gare", color="nombre_objets",
-                        size="frequentation_" + annee_selectionnee, color_continuous_scale=px.colors.cyclical.IceFire,
-                        size_max=30, zoom=11, mapbox_style="carto-positron")
-fig.update_layout(title="Répartition des objets trouvés par gare et fréquentation en " + annee_selectionnee)
-st.plotly_chart(fig)
+
+# Création de la carte de Paris avec les barres représentant le ratio
+view_state = pdk.ViewState(
+    longitude=df_gare['lon'].mean(),
+    latitude=df_gare['lat'].mean(),
+    zoom=12,
+    pitch=45
+)
+layer = pdk.Layer(
+    'ColumnLayer',
+    data=df_gare,
+    get_position='[lon, lat]',
+    get_elevation='ratio_freq_objets',
+    elevation_scale=10,
+    # get_fill_color='[255 * (1 - ratio_freq_objets), 0, 255 * ratio_freq_objets]',
+    get_fill_color='[255, ratio_freq_objets, ratio_freq_objets, 175]',
+    pickable=True,
+    auto_highlight=True,
+    extruded=True,
+    coverage=0.2
+)
+tooltip = {
+    "html": "<b>Gare :</b> {gare}<br/><b>Nombre d'objets trouvés sur 1 million de voyageurs :</b> {ratio_freq_objets} objets",
+    "style": {"backgroundColor": "black", "color": "white"}
+}
+r = pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip=tooltip)
+st.pydeck_chart(r)
